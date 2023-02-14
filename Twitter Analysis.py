45379423bc47ba5cc2bb1dff719360767c7cb19c
VindhyaKabeerdass <90118998+VindhyaKabeerdass@users.noreply.github.com>
2023-02-13 22:36:18 -0500

    Update Twitter Analysis.py

--- Twitter Analysis.py
+++ Twitter Analysis.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
 
-
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.model_selection import StratifiedKFold, cross_validate
@@ -18,7 +17,6 @@ from sklearn.neural_network import MLPClassifier
 from sklearn import semi_supervised
 from sklearn import metrics
 
-
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
@@ -30,7 +28,7 @@ st.sidebar.title("Semi-supervised learning")
 st.subheader('Assignment 3(TIMG 5301 - Group 8)')
 st.markdown(f'<h1 style="color:#00008B;font-size:20px;">{"Vindhya"}</h1>',unsafe_allow_html=True)
 
-############################### 2. Data understanding and preparation ############################
+############################### Data understanding and preparation ############################
 
 def load_corpus():
     corpus = pd.read_csv("ExtractedTweets.csv")
@@ -43,7 +41,7 @@ def load_corpus():
 def load_labels():
     return pd.read_csv("LabelledData.csv")
 
-# 2.c Load the corpus of tweets and convert it to a data frame
+# Load the corpus of tweets and convert it to a data frame
 
 dataFrame = load_corpus()
 labels = load_labels()
@@ -53,18 +51,15 @@ if st.sidebar.checkbox("Show Unlabelled Data"):
     st.subheader("Unlabelled Data")
     st.write(dataFrame)
 
- #2.d Label a subset of the tweets as positive (idea) or negative (not an idea)
+ # Label a subset of the tweets as positive (idea) or negative (not an idea)
 
 dataFrame['label'] = -1 
 for _, row in labels.iterrows():
     dataFrame.loc[row['id'], 'label'] = row['label']
 
-
-
 if st.sidebar.checkbox("Show corpus"):
     st.subheader("Corpus")
-    st.dataframe(dataFrame)
-    
+    st.dataframe(dataFrame)   
     
 counts = dataFrame['label'].value_counts()
 fig, ax = plt.subplots(figsize=(6.4, 2.4))
@@ -77,8 +72,6 @@ ax.set_title('Class imbalance')
 Good = counts[1]
 Bad = counts[0]
 
-#2.e Determine the ratio of negative to positive tweets. Is the dataset balanced or imbalanced?
-
 if st.sidebar.checkbox('Check imbalance'):
     st.subheader('Class imbalance')
     st.pyplot(fig)
@@ -89,7 +82,7 @@ if st.sidebar.checkbox('Check imbalance'):
     bad_r = Bad/Good
     st.write('Ratio of negative to positive ideas in the labelled data: ' , round(bad_r, 2))
     
-#2.f Definition of stop words.    
+# Defining stop words.    
     
 user_stopwords = st.sidebar.text_area("Stopwords (one per line)",
     on_change=lambda: st.session_state.clear())
@@ -98,9 +91,6 @@ def read_stopwords(file):
     file = open(file, 'r')
     return [w.strip() for w in file.read().split('\n')]
 additionalStopwords = ["pollution","http"," idea","https","pollutions","ideas"]
-
-#2.g Create a term-document matrix that shows the terms used as features and their TF-IDF values in each document
-
 from sklearn.feature_extraction.text import CountVectorizer
 preprocessor = Pipeline([
     ('vectorizer', CountVectorizer(stop_words=['english','additionalStopwords','user_stopwords'], max_features=100, min_df=2)),
@@ -110,22 +100,21 @@ vectorizer = preprocessor.named_steps['vectorizer']
 vocab = vectorizer.get_feature_names_out()
 tdm = pd.DataFrame(tfidf.toarray().T, index=vocab)
 
-
-
 if st.sidebar.checkbox('Show term-document matrix'):
     st.subheader('Term-document matrix')
     st.write('The rows are words and the columns are documents.')
     st.dataframe(tdm)
     
-#######################################################3. Modelling ############################################
+####################################################### Modelling ############################################
 
-# 3.a Create a base estimator using the Logistic Regression classifier
+# Create a base estimator using the Logistic Regression classifier
 
-#3.b Define a self-training semi-supervised classifier that uses the base estimator
+# Define a self-training semi-supervised classifier that uses the base estimator
 
-#3.c Create a pipeline that preprocesses the tweets
+# Create a pipeline that preprocesses the tweets
 
 # Self-training requires a base estimator
+
 base_estimator = LogisticRegression(penalty='l2', class_weight='balanced')
 classifier = semi_supervised.SelfTrainingClassifier(base_estimator)
 model = Pipeline(steps=[
@@ -152,7 +141,8 @@ def predict(dataFrame, model):
 
 model.fit(dataFrame['content'], dataFrame['label'])
 
-#3.e Apply the pipeline to the corpus and show the predicted labels
+# Apply the pipeline to the corpus and show the predicted labels
+
 if st.sidebar.checkbox("Predicted labels"):
     st.subheader("Predicted labels")
     # Predict the labels for the unlabeled tweets
@@ -164,8 +154,8 @@ if st.sidebar.checkbox("Predicted labels"):
     st.write("This is a count of the predicted labels:")
     st.write(dataFrame['predicted'].value_counts())
 
- #3.f Save the model to a file, and create a second Streamlit application that loads the model you saved, and asks the user to
-#enter a tweet and reports back the predicted label.
+ # Create a Streamlit application that loads the model you saved, and asks the user to
+ #enter a tweet and reports back the predicted label.
  
 filename = "model_file.pkl"
 pickle.dump(model,open(filename,'wb'))
@@ -175,8 +165,7 @@ if st.sidebar.checkbox("Save the model"):
     # save the model to disk
     st.write("Great! the file successfully got Saved ", filename)
  
- ######################################## 4.Evaluation and interpretation #############################################
- 
+ ######################################## Evaluation and interpretation #############################################
  
  
 if st.sidebar.checkbox("Evaluate the Model"):
@@ -187,7 +176,7 @@ if st.sidebar.checkbox("Evaluate the Model"):
     #precision = precision_score(corpus['label'], corpus['predicted'], average= None)
     st.text('Model Report:\n ' + classification_report(dataFrame['label'] , dataFrame['predicted']))
     
- #4.a Use stratified k-fold cross-validation to evaluate the model.
+ # stratified k-fold cross-validation to evaluate the model.
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 scores = cross_validate(model,dataFrame.loc[0:99,'content'], dataFrame.loc[0:99,'label'], cv=cv, n_jobs=-1,
     scoring=['accuracy', 'roc_auc', 'precision', 'recall', 'f1'])
@@ -195,7 +184,6 @@ scores = cross_validate(model,dataFrame.loc[0:99,'content'], dataFrame.loc[0:99,
 if st.sidebar.checkbox("Show model performance Stratified k-fold"):
     st.subheader('Model performance Stratified k-fold')
     st.write("The table shows the performance of the logistic regression classifier on the training data.")
- # 4.c  Performance scores for each fold
     st.write("Scores for each fold (only positive class):")
     data_scores = pd.DataFrame(scores).transpose()
     data_scores['mean'] = data_scores.mean(axis=1)
